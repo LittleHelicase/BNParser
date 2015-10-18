2c4f5550bbc8dfcadb0a3705e2736c589a90516c
Maximilian Klein <dasmaxl@gmail.com>
2015-10-18 10:45:29 +0200

    (feature) Use PegJS to parse BNs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
+var gulp = require('gulp')
+var babel = require('gulp-babel')
+var peg = require('gulp-peg')
+var gutil = require('gulp-util')
 
-gulp.task('default', function () {
-    return gulp.src('src/**/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('lib'));
-});
+gulp.task('babel', function () {
+  return gulp.src('src/**/*.js')
+      .pipe(babel())
+      .pipe(gulp.dest('lib'))
+})
+
+gulp.task('grammar', function () {
+  return gulp.src('src/grammar.peg')
+    .pipe(peg().on('error', gutil.log))
+    .pipe(gulp.dest('lib'))
+})
+
+gulp.task('default', ['babel', 'grammar'])
